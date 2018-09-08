b4400b084834835588618daede3d0827b4fb9752
christopherhui <christophergkhui@gmail.com>
2018-09-08 15:34:40 -0700

    Added handle keys -- todo later (pun intended).

--- src/ui/ToDoList.java
+++ src/ui/ToDoList.java
@@ -4,11 +4,14 @@ import java.util.*;
 
 public class ToDoList {
     public void welcomeSign() {
-        System.out.println("Welcome to your todo list.\nOptions: Press 1 to find your tasks, " +
-                "Press 2 to add a task. " + "Press 3 to remove a task.");
+        System.out.println("Welcome to your todo list.");
     }
 
     public void generateOptions() {
+        System.out.println("Options: Press 1 to find your tasks, " +
+        "Press 2 to add a task. " + "Press 3 to remove a task.");
+    }
+    public void handleKeys() {
     }
 
     public static void main(String[] args) {
@@ -16,6 +19,7 @@ public class ToDoList {
 
         ToDoList tdl = new ToDoList();
         tdl.welcomeSign();
+        tdl.generateOptions();
 
         options.close();
     }
