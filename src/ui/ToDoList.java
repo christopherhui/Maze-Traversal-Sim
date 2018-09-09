85684845bc5281cda0127f4931cce7e3d84921d3
christopherhui <christophergkhui@gmail.com>
2018-09-08 22:16:58 -0700

    Private static methods invoked.

--- src/ui/ToDoList.java
+++ src/ui/ToDoList.java
@@ -3,25 +3,24 @@ package ui;
 import java.util.*;
 
 public class ToDoList {
-    public void welcomeSign() {
+    private static void welcomeSign() {
         System.out.println("Welcome to your todo list.");
     }
 
-    public void generateOptions() {
+    private static void generateOptions() {
         System.out.println("Options: Press 1 to find your tasks, " +
         "Press 2 to add a task. " + "Press 3 to remove a task.");
     }
-    public void handleKeys() {
+    private static void handleKeys() {
         System.out.println("--- Handles Keys ---");
     }
 
     public static void main(String[] args) {
         Scanner options = new Scanner(System.in);
 
-        ToDoList tdl = new ToDoList();
-        tdl.welcomeSign();
-        tdl.generateOptions();
-        tdl.handleKeys();
+        welcomeSign();
+        generateOptions();
+        handleKeys();
 
         options.close();
     }
