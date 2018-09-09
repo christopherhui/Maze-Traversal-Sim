4fb59f5571a32137018d9402b2df0e497a1c2787
christopherhui <christophergkhui@gmail.com>
2018-09-09 15:26:17 -0700

    Public methods for Task

--- src/ui/Task.java
+++ src/ui/Task.java
@@ -1,13 +1,29 @@
 package ui;
 
 public class Task {
-    String taskDesc;
-    String taskDue;
-    String taskCategory;
+    private String taskDesc;
+    private String taskDue;
+    private String taskCategory;
 
     Task(String desc, String date, String category) {
         this.taskDesc = desc;
         this.taskDue = date;
         this.taskCategory = category;
+    }
+
+    public String get_task() {
+        return taskDesc;
+    }
+
+    public String get_due_date() {
+        return taskDue;
+    }
+
+    public String get_task_category() {
+        return taskCategory;
+    }
+
+    public void change_date(String date) {
+        taskDue = date;
     }
 }
